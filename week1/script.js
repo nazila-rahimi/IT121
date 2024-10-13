1090a3d7a1201b33b065b6dad5a509235eae8e62
Nazila Rahimi <153054137+nazila-rahimi@users.noreply.github.com>
2024-10-13 05:26:41 +0000

    Add working profile introduction and update student data display

---
+++ week1/script.js
@@ -0,0 +1,9 @@
+var firstName = "Nazila";
+var lastName = "Rahimi";
+var yearsOfStudy = 2;
+var goal = "become a web developer";
+
+document.getElementById("profile").innerHTML = 
+    "My name is " + firstName + " " + lastName + 
+    ". I have been at SCC for " + yearsOfStudy + 
+    " years and plan to " + goal + " after completing my courses.";
